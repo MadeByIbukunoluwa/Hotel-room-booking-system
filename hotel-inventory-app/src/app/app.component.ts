c605349b02eac4a66f36633115fc728ef2a7b24d
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-12-07 19:58:43 +0100

    implemented localstorage with injection tokens

--- hotel-inventory-app/src/app/app.component.ts
+++ hotel-inventory-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Optional, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, Optional, ViewChild, ViewContainerRef } from '@angular/core';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { LoggerService } from './logger.service';
-
+import {LocalStorageToken} from './localstorage.token'
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
@@ -14,7 +14,8 @@ export class AppComponent implements OnInit {
   title = 'hotel-inventory-app';
   role = 'Admin'
 
-  constructor(@Optional() private loggerService: LoggerService) {
+  constructor(@Optional() private loggerService: LoggerService,
+  @Inject(LocalStorageToken) private localStorage:Storage) {
 
   }
   @ViewChild(`name`,{static:true}) name!:ElementRef;
@@ -28,6 +29,7 @@ export class AppComponent implements OnInit {
 ngOnInit() {
   this.loggerService?.log(`AppComponent.ngOnInit()`)
   this.name.nativeElement.innerText = 'Ibukunoluwa Hotel'
+  this.localStorage.setItem('name','Ibukunoluwa Hotel')
 }
 
 }
--- hotel-inventory-app/src/app/components/rooms/services/rooms.service.ts
+++ hotel-inventory-app/src/app/components/rooms/services/rooms.service.ts
@@ -1,56 +1,61 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { RoomList } from '../rooms';
+import { environment } from 'src/environment/environment';
+import { APP_SERVICE_CONFIG } from 'src/app/AppConfig/appconfig.service';
+import { AppConfig } from 'src/app/AppConfig/appconfig.interface';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoomsService {
-  roomlist :RoomList[]= [
-      {
-        roomNumber: 33,
-        roomType:'Deluxe Room',
-        amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 500,
-        photos : '',
-        checkinTime: new Date('25-Nov-2022'),
-        checkoutTime: new Date('26-Nov-2022'),
-        rating:4.7
+  roomlist: RoomList[] = [
+    {
+      roomNumber: 33,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: '',
+      checkinTime: new Date('25-Nov-2022'),
+      checkoutTime: new Date('26-Nov-2022'),
+      rating: 4.7,
     },
-      {
-        roomNumber: 30,
-        roomType:'Deluxe Room',
-        amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 590,
-        photos : '',
-        checkinTime: new Date('25-Nov-2022'),
-        checkoutTime: new Date('26-Nov-2022'),
-        rating:4.7
+    {
+      roomNumber: 30,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 590,
+      photos: '',
+      checkinTime: new Date('25-Nov-2022'),
+      checkoutTime: new Date('26-Nov-2022'),
+      rating: 4.7,
     },
-      {
-        roomNumber: 24,
-        roomType:'Deluxe Room',
-        amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 500,
-        photos : '',
-        checkinTime: new Date('25-Nov-2022'),
-        checkoutTime: new Date('26-Nov-2022'),
-        rating:4.4
+    {
+      roomNumber: 24,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: '',
+      checkinTime: new Date('25-Nov-2022'),
+      checkoutTime: new Date('26-Nov-2022'),
+      rating: 4.4,
     },
-      {
-        roomNumber: 53,
-        roomType:'Private Suite',
-        amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 1000,
-        photos : '',
-        checkinTime: new Date('25-Nov-2022'),
-        checkoutTime: new Date('26-Nov-2022'),
-        rating:4
+    {
+      roomNumber: 53,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos: '',
+      checkinTime: new Date('25-Nov-2022'),
+      checkoutTime: new Date('26-Nov-2022'),
+      rating: 4,
     },
-
-  ]
-  constructor() {
-      console.log('Room services initialized')
-   }
-   getRooms() {
-    return this.roomlist 
-   }
+  ];
+  constructor(@Inject(APP_SERVICE_CONFIG) private config: AppConfig) {
+    console.log(this.config.apiEndpoint)
+    console.log('Room services initialized');
+  }
+  getRooms() {
+    return this.roomlist;
+  }
 }
+
+
